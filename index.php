295ab9c5c413426fb6eac5c3b82756bb85e8f178
mbi <m.bietenbeck@laudert.de>
2013-10-25 16:51:22 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+include_once "KartenController.php";
+include_once "includes/header.php";
+
+$cardFunction = new CardFunctions();
+?>
+<script>
+    $("#button").click(function(){
+      $.ajax({url:"insertD",success:function(result){
+        $("#div1").html(result);
+      }});
+    }); >
+</script>
+
+
+
+<div><?php $cardFunction->render(); ?></div>
+
+
+
