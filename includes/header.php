f14cece4c815621cf529bdd535ffdb206cd7c2d1
mbi <m.bietenbeck@laudert.de>
2013-10-28 09:26:44 +0100

    css+loadingscreen

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <head>
 <script language="JavaScript" src="/src/js/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/src/css/style.css">
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 </head>
--- index.php
+++ index.php
@@ -9,17 +9,31 @@ $cardFunction = new CardFunctions();
 ?>
 <script>
     function addData(){
+        showLoadScreen();
       $.ajax({url:"cardsHandler.php?fn=addNewPrices",type:"POST",success:function(result){
         $("#renderTable").html(result);
+        hideLoadScreen();
       }});
     }
     function refreshData(){
+      showLoadScreen();
       $.ajax({url:"cardsHandler.php?fn=render",type:"POST",success:function(result){
         $("#renderTable").html(result);
+        hideLoadScreen();
       }});
     }
+    function showLoadScreen(){
+        $("#loadScreen").css("width","100%");
+        $("#loadScreen").css("height","100%");
+        $("#loadScreen").css("z-index","100");
+    }
+    function hideLoadScreen(){
+        $("#loadScreen").css("width","0px");
+        $("#loadScreen").css("height","0px");
+        $("#loadScreen").css("z-index","0");
+    }
 </script>
-
+<div id="loadScreen"></div>
 <div id="renderTable"><?php $cardFunction->render(); ?></div>
 <button class="addButton" onclick="addData()">Werte Übertragen</button>
 <button class="addButton" onclick="refreshData()">REFRESH</button>
