b349fc908d6172edeab38d5a64b95c60e198c487
mbi <m.bietenbeck@laudert.de>
2013-10-29 17:24:37 +0100

    move js into seperated file

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <head>
 <script language="JavaScript" src="/src/js/jquery.min.js"></script>
+<script language="JavaScript" src="/src/js/main.js"></script>
 <link rel="stylesheet" type="text/css" href="/src/css/style.css">
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 </head>
--- index.php
+++ index.php
@@ -7,58 +7,6 @@ include_once "includes/header.php";
 
 $cardFunction = new CardFunctions();
 ?>
-<script>
-    function addData(){
-        showLoadScreen();
-      $.ajax({url:"cardsHandler.php?fn=addNewPrices",type:"POST",success:function(result){
-        $("#renderTable").html(result);
-        hideLoadScreen();
-      }});
-    }
-    function addCard(){
-        showLoadScreen();
-        var url = document.getElementById("newURL").value;
-        $.ajax({url:"cardsHandler.php?fn=addNewURL&param=" + url,type:"POST",success:function(result){
-            $("#renderTable").html(result);
-            hideLoadScreen();
-        }});
-        document.getElementById("newURL").value = "";
-    }
-    function removeCard(id){
-        showLoadScreen();
-        $.ajax({url:"cardsHandler.php?fn=deleteCard&param=" + id,type:"POST",success:function(result){
-            $("#renderTable").html(result);
-            hideLoadScreen();
-        }});
-    }
-    function refreshData(){
-      showLoadScreen();
-      $.ajax({url:"cardsHandler.php?fn=render",type:"POST",success:function(result){
-        $("#renderTable").html(result);
-        hideLoadScreen();
-      }});
-    }
-    function showLoadScreen(){
-        $("#loadScreen").css("width","100%");
-        $("#loadScreen").css("height","100%");
-        $("#loadScreen").css("z-index","100");
-    }
-    function hideLoadScreen(){
-        $("#loadScreen").css("width","0px");
-        $("#loadScreen").css("height","0px");
-        $("#loadScreen").css("z-index","0");
-    }
-    function hoverPic(pic,event){
-        $("#hoverDiv").css("background","url('"+pic+"') no-repeat scroll 0 0 / 100% auto");
-        $("#hoverDiv").css("display","block");
-        $("#hoverDiv").css("margin-top",event.pageY-100);
-        $("#hoverDiv").css("margin-left",event.pageX-350);
-    }
-    function hidePic(){
-        $("#hoverDiv").css("display","none");
-    }
-
-</script>
 <div id="loadScreen"></div>
 
 <div id="content">
